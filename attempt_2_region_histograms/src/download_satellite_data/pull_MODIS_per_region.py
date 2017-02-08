be9e9daa832901fbcd9f54e8edad1a8c922bf39f
rapigan <rapigan@gmail.com>
2017-02-08 14:04:29 -0800

    photosynthesis, temperature modis pull

--- attempt_2_region_histograms/src/download_satellite_data/pull_MODIS_per_region.py
+++ attempt_2_region_histograms/src/download_satellite_data/pull_MODIS_per_region.py
@@ -32,7 +32,7 @@ print 'initializing earth engine connection...'
 start = time.time()
 ee.Initialize()
 regions = ee.FeatureCollection("ft:133FLgnCJZsRswd2sb7Sp-od0Z90nB6P1qHuUDe57")
-MODIS_collection = ee.ImageCollection('MODIS/MOD09A1')
+
 cropland = ee.Image("/Users/rapigan/Downloads/cropland_hybrid_14052014v8/out.tif")
 #cropland = ee.Image("users/rpryzant/IIASA_IFPRI")
 def mask_cropland(image):
@@ -42,16 +42,19 @@ print 'done. took {:.2f} seconds'.format(time.time() - start)
 
 
 
-def append_band(cur, prev):
-    """ takes all the bands from a stack of images and combines them into one meta-image 
-        with many bands
-        e.g. given images I1:[band1, band2] and I2:[band1, band2], this function will return
-             Iappend:[band1, band2, band1_1, band2_1] with bandX_1 coming from I2
-    """
-    prev = ee.Image(prev)
-    cur = cur.select([0, 1, 2, 3, 4, 5, 6])
-    accum = ee.Algorithms.If(ee.Algorithms.IsEqual(prev, None), cur, prev.addBands(ee.Image(cur)))
-    return accum
+def appender(bands):
+    def _append_band(cur, prev):
+        """ takes all the bands from a stack of images and combines them into one meta-image 
+            with many bands
+            e.g. given images I1:[band1, band2] and I2:[band1, band2], this function will return
+                 Iappend:[band1, band2, band1_1, band2_1] with bandX_1 coming from I2
+        """
+        prev = ee.Image(prev)
+        cur = cur.select(bands)
+        accum = ee.Algorithms.If(ee.Algorithms.IsEqual(prev, None), cur, prev.addBands(ee.Image(cur)))
+        return accum
+
+    return _append_band
 
 
 def export_image(img, folder, name, scale):
@@ -69,11 +72,15 @@ def export_image(img, folder, name, scale):
 
 
 seasons = {y: ('%s-06-01' % y, '%s-03-01' % (y+1)) for y in range(2007, 2017) if y != 2009}
-folder = 'test_%s'
-scale = 500     # modis is 500m resolution
+scale = 500     # modis is 500m resolution. downsample coarser images to this
+
+
+############  1: surface reflectance
+folder = 'reflectance_%s'
+MODIS_collection = ee.ImageCollection('MODIS/MOD09A1')
 for season, (start, end) in seasons.iteritems():
     modis_images = MODIS_collection.filterDate(start, end).map(mask_cropland)
-    modis_meta_image = modis_images.iterate(append_band)
+    modis_meta_image = modis_images.iterate(appender([0,1,2,3,4,5,6]))    # sr bands 1-7
     modis_meta_image = ee.Image(modis_meta_image)
     for region_name in sf.surveyed_regions(season):
         region = regions.filterMetadata('name', 'equals', region_name)
@@ -81,4 +88,26 @@ for season, (start, end) in seasons.iteritems():
 
 
 
+###########   2: temperature
+folder = 'temp_%s'
+MODIS_collection = ee.ImageCollection('MODIS/MYD11A2')
+for season, (start, end) in seasons.iteritems():
+    modis_images = MODIS_collection.filterDate(start, end).map(mask_cropland)
+    modis_meta_image = modis_images.iterate(appender([0,4]))             # day and night tmp
+    modis_meta_image = ee.Image(modis_meta_image)
+    for region_name in sf.surveyed_regions(season):
+        region = regions.filterMetadata('name', 'equals', region_name)
+        export_image(modis_meta_image.clip(region), folder % season, region_name, scale)
 
+
+
+###########  3: primary productivity
+folder = 'gpp_%s'
+MODIS_collection = ee.ImageCollection('MODIS/006/MYD17A2H')
+for season, (start, end) in seasons.iteritems():
+    modis_images = MODIS_collection.filterDate(start, end).map(mask_cropland)
+    modis_meta_image = modis_images.iterate(appender([0,1]))             # gpp and net photosythesis (gpp - maintenence respo)
+    modis_meta_image = ee.Image(modis_meta_image)
+    for region_name in sf.surveyed_regions(season):
+        region = regions.filterMetadata('name', 'equals', region_name)
+        export_image(modis_meta_image.clip(region), folder % season, region_name, scale)
