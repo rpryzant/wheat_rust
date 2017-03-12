4ab6f59c12028af240095e9d9882025739eef05c
joyhsu0504 <joyhsu0504@gmail.com>
2017-03-11 18:53:24 -0800

    region

--- attempt_2_region_histograms/src/download_satellite_data/modis.py
+++ attempt_2_region_histograms/src/download_satellite_data/modis.py
@@ -33,27 +33,31 @@ def download_image (img_collection, start, end, region, bands, folderName, fileN
 		'scale':500,
 		'maxPixels':966050808
 	}
-	print jsonTest
+	#print jsonTest
 	task = ee.batch.Export.image(img, taskName, jsonTest)
 	task.start()
 	while task.status()['state'] == 'RUNNING':
 		print 'Running...'
-	print 'Done.', task.status()
+	#print 'Done.', task.status()
+	print fileName + ' done.'
 
 ee.Initialize()
 regions = ee.FeatureCollection('ft:133FLgnCJZsRswd2sb7Sp-od0Z90nB6P1qHuUDe57')
-cropland = ee.Image("mask.tif")
+cropland = ee.Image("users/rpryzant/IIASA_IFPRI")
 start = time.time()
 bandsInput = raw_input("Bands(sr, tmp, gpp): ")
 yearInput = int(raw_input("Year(ex. 2016): "))
 
 sys.path.insert(0, os.path.abspath("../.."))
 sf = SurveyFeaturizer(sys.argv[1], sys.argv[2])
+print 'Starting region featurizer...'
 seasons = {y: ('%s-06-01' % y, '%s-03-01' % (y+1)) for y in range(2007, 2017) if y == yearInput}
 for season, (start, end) in seasons.iteritems():
 	regions_data = sf.surveyed_regions(season)
-	print 'data_%s = %s' % (str(season), str([[r, start, end, season] for r in regions_data]))
+	#print 'data_%s = %s' % (str(season), str([[r, start, end, season] for r in regions_data]))
 	exec('year = %s' % (str([[r, start, end, season] for r in regions_data])))
+
+print 'Completed.'
 
 if bandsInput == "sr":
 	bands = [0,1,2,3,4,5,6]
@@ -64,6 +68,8 @@ if bandsInput == "tmp":
 if bandsInput == "gpp":
 	bands = [0]
 	collection = ee.ImageCollection('MODIS/006/MYD17A2H')
+
+print 'Downloading for ' + str(yearInput) + '...'
 
 for year_data in year:
 	region      = year_data[0]
