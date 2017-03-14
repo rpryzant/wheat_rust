992a10ea4c338ddc9437a8c1e06c64899b29711c
rapigan <rapigan@gmail.com>
2017-03-14 11:17:34 -0700

    refactor

--- paper_prep/src/analysis/roc_plotter.py
+++ paper_prep/src/analysis/roc_plotter.py
@@ -3,7 +3,6 @@
 Given the output of a model run, plots the saved roc curve
 
 === USAGE
-python roc_plotter.py [outfile]
    NOTE: only the first curve will be plotted
 """
 import sys
@@ -11,22 +10,25 @@ import matplotlib.pyplot as plt
 import json
 
 
-out = next(open(sys.argv[1]))
-try:
-    out_data = json.loads(out)
-except:
-    out_data = eval(out)
-
-curve = eval(out_data['result']['rocs'])
-
-[fpr, tpr, thresholds] = curve
+def extract_roc(s):
+    try:
+        out_data = json.loads(s)
+    except:
+        out_data = eval(s)
+    return eval(out_data['result']['rocs']), out_data['result']['auc']
 
 
 
+[fpr, tpr, thresholds], auc = extract_roc(next(open(sys.argv[1])))
 
+#[fpr_base, tpr_base, thresholds] = extract_roc(next(open(sys.argv[2])))
+
+#[fpr_hist, tpr_hist, thresholds] = extract_roc(next(open(sys.argv[3])))
+
+print 'AUC: ', auc
 plt.figure(1)
 plt.plot([0, 1], [0, 1], 'k--')
-plt.plot(fpr, tpr, label='Best deep features')
+plt.plot(fpr, tpr)#, label='Best deep features')
 plt.xlabel('False positive rate')
 plt.ylabel('True positive rate')
 plt.title('ROC curve')
